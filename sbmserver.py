123eb3cc7f533975369a13bfb110162f5de95444
simon <heyyooman@163.com>
2021-01-27 21:34:52 +0800

    flask 0001

--- sbmserver.py
+++ sbmserver.py
@@ -5,13 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello sbm'
+    vpath = sbm_win.VenvOperation.get_venv_path()
+    return 'hello sbm\n',vpath
 
 @app.route('/VenvList')
 def get_venvlist():
-    venv_name = sbm_win.VenvOperation.get_venv_list()
-    venv_path = sbm_win.VenvOperation.get_venv_path()
-    return jsonify({'venv_name': venv_name,'venv_path':venv_path})
+    pass
+    # venv_name = sbm_win.VenvOperation.get_venv_list()
+    # venv_path = sbm_win.VenvOperation.get_venv_path()
+    # return jsonify({'venv_name': venv_name,'venv_path':venv_path})
 
 if __name__ == '__main__':
     app.run(debug=True)
